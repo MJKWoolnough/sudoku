2c6e46eaecd7f97446eff4713de08bb1dac192c1
Michael Woolnough <michael.woolnough@gmail.com>
2014-11-08 13:03:52 +0000

    Modified license

--- sudoku.go
+++ sudoku.go
@@ -1,28 +1,3 @@
-// Copyright (c) 2013 - Michael Woolnough <michael.woolnough@gmail.com>
-//
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice, this
-//    list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-//    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-// Sudoku is a generic solver for sudoku type puzzles. It can solve a puzzle of any shape and size.
 package sudoku
 
 var (
@@ -194,10 +169,7 @@ func (s *sudoku) solve() bool {
 			break
 		}
 	}
-	if pos == l {
-		return true
-	}
-	for {
+	for pos < l {
 		if len(possibilities[pos]) == 0 {
 			s.data[pos] = 0
 			for pos--; pos >= 0 && len(possibilities[pos]) == 0; pos-- {
@@ -206,7 +178,7 @@ func (s *sudoku) solve() bool {
 				}
 			}
 			if pos < 0 {
-				break //failure
+				return false
 			}
 			continue
 		}
@@ -218,9 +190,6 @@ func (s *sudoku) solve() bool {
 				break
 			}
 		}
-		if pos == l {
-			return true //success
-		}
 	}
-	return false
+	return true
 }
