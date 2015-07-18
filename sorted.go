3383a341e69b3f535df567b6ac34ad214e6b4076
Michael Woolnough <michael.woolnough@gmail.com>
2015-07-18 22:18:55 +0100

    Corrected Sorted contraint

--- sorted.go
+++ sorted.go
@@ -1,22 +1,48 @@
 package sudoku
 
-/*type Sorted []int
+type Sorted []int
 
 func (s Sorted) Constrain(su *Sudoku, pos int, marked []bool) bool {
-	if slicePos([]int(s), pos) == -1 {
+	sp := slicePos([]int(s), pos)
+	if sp == -1 {
 		return true
 	}
-	num := 0
-	for _, p := range s {
-		if mp := su.data[p]; mp != 0 {
-			if mp > num {
-				num = mp
-			} else {
-				return false
-			}
-		} else if mp == pos {
-
+	min := 1
+	nums := make([]int, len(s))
+	for n, p := range s {
+		nums[n] = su.data[p]
+	}
+	f := false
+	for i := 0; i < sp; i++ {
+		if mp := su.data[s[i]]; mp == 0 {
+			min++
+		} else if mp+1 > min {
+			min = mp + 1
+		} else if f {
+			return false
 		}
+		f = true
+	}
+	max := su.chars
+	f = false
+	for i := len(s) - 1; i > sp; i-- {
+		if mp := su.data[s[i]]; mp == 0 {
+			max--
+		} else if mp-1 < max {
+			max = mp - 1
+		} else if f {
+			return false
+		}
+		f = true
+	}
+	if max-min < 0 {
+		return false
+	}
+	for i := 1; i < min; i++ {
+		marked[i] = true
+	}
+	for i := su.chars; i > max; i-- {
+		marked[i] = true
 	}
 	return true
-}*/
+}
