7e3e242a329ced2b4fc965bc45143b1ec41a6c63
Michael Woolnough <michael.woolnough@gmail.com>
2015-07-25 08:00:12 +0100

    Added sumdoku test

--- uniquesum_test.go
+++ uniquesum_test.go
@@ -1,8 +1,11 @@
 package sudoku
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestKiller(t *testing.T) {
+func TestSumdoku(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
 		start, solution []int
@@ -10,7 +13,8 @@ func TestKiller(t *testing.T) {
 		chars           int
 		constraints     []Constraint
 	}{
-		{make([]int, 81),
+		{
+			make([]int, 81),
 			[]int{
 				6, 2, 9, 1, 4, 3, 8, 5, 7,
 				3, 5, 7, 9, 6, 8, 4, 2, 1,
@@ -53,13 +57,69 @@ func TestKiller(t *testing.T) {
 				UniqueSum{[]int{70, 79}, 8},
 				UniqueSum{[]int{71, 80}, 12},
 				UniqueSum{[]int{77, 78}, 4},
-			)}}
+			),
+		},
+		{
+			make([]int, 81),
+			[]int{
+				2, 6, 7, 9, 5, 1, 3, 4, 8,
+				3, 5, 4, 7, 2, 8, 1, 6, 9,
+				8, 1, 9, 6, 3, 4, 2, 5, 7,
+				9, 3, 6, 2, 4, 7, 5, 8, 1,
+				5, 2, 8, 1, 9, 6, 7, 3, 4,
+				4, 7, 1, 3, 8, 5, 9, 2, 6,
+				1, 4, 5, 8, 7, 3, 6, 9, 2,
+				6, 8, 2, 5, 1, 9, 4, 7, 3,
+				7, 9, 3, 4, 6, 2, 8, 1, 5,
+			},
+			true,
+			9,
+			append(s9,
+				UniqueSum{[]int{0, 9, 18, 27}, 22},
+				UniqueSum{[]int{1, 2, 3, 10}, 27},
+				UniqueSum{[]int{4, 5, 6}, 9},
+				UniqueSum{[]int{7, 16}, 10},
+				UniqueSum{[]int{8, 17}, 17},
+				UniqueSum{[]int{11, 12, 21}, 17},
+				UniqueSum{[]int{13, 22, 31}, 9},
+				UniqueSum{[]int{14, 15, 23, 24}, 15},
+				UniqueSum{[]int{19, 28}, 4},
+				UniqueSum{[]int{20, 29, 38}, 23},
+				UniqueSum{[]int{25, 26, 34}, 20},
+				UniqueSum{[]int{30, 39}, 3},
+				UniqueSum{[]int{32, 33}, 12},
+				UniqueSum{[]int{35, 43, 44}, 8},
+				UniqueSum{[]int{36, 37, 45, 54}, 12},
+				UniqueSum{[]int{40, 41, 49}, 23},
+				UniqueSum{[]int{42, 51, 60}, 22},
+				UniqueSum{[]int{46, 55}, 11},
+				UniqueSum{[]int{47, 48, 56}, 9},
+				UniqueSum{[]int{50, 57, 58, 59}, 23},
+				UniqueSum{[]int{52, 53, 61, 70}, 24},
+				UniqueSum{[]int{62, 71, 79, 80}, 11},
+				UniqueSum{[]int{63, 64, 72, 73}, 30},
+				UniqueSum{[]int{65, 66, 67}, 8},
+				UniqueSum{[]int{68, 69}, 13},
+				UniqueSum{[]int{74, 75}, 7},
+				UniqueSum{[]int{76, 77, 78}, 16},
+			),
+		},
+	}
 	for i, test := range tests {
 		solved := Solve(test.start, test.chars, test.constraints)
 		if test.solveable {
 			if solved {
 				for j, num := range test.start {
 					if num != test.solution[j] {
+						fmt.Println(test.start[:9])
+						fmt.Println(test.start[9:18])
+						fmt.Println(test.start[18:27])
+						fmt.Println(test.start[27:36])
+						fmt.Println(test.start[36:45])
+						fmt.Println(test.start[45:54])
+						fmt.Println(test.start[54:63])
+						fmt.Println(test.start[63:72])
+						fmt.Println(test.start[72:])
 						t.Errorf("solution found does not match solution given for test %d", i+1)
 						break
 					}
